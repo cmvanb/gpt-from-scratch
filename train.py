bac982b0c2e52545a513fc288d44c9ed31c01f2d
Casper <draakie@protonmail.com>
2024-02-08 00:26:34 +0100

    [WIP]

---
+++ train.py
@@ -0,0 +1,76 @@
+import torch
+
+def main():
+    with open('input.txt', 'r', encoding='utf-8') as f:
+        text = f.read()
+
+    # print('length of dataset in characters: ', len(text))
+
+    # let's look at the first 1000 characters
+    # print(text[:1000])
+
+    # here are all the unique characters that occur in this text
+    chars = sorted(list(set(text)))
+    # vocab_size = len(chars)
+    # print(''.join(chars))
+    # print(vocab_size)
+
+    # data = torch.tensor(encode())
+
+    # create a mapping from characters to integers
+    stoi = { ch:i for i,ch in enumerate(chars) }
+    itos = { i:ch for i,ch in enumerate(chars) }
+    encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
+    decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+
+    # print(encode("hii there"))
+    # print(decode(encode("hii there")))
+
+    data = torch.tensor(encode(text), dtype=torch.long)
+    print(data.shape, data.dtype)
+    # print(data[:1000])
+
+    n = int(0.9*len(data)) # first 90% is training data
+    train_data = data[:n]
+    val_data = data[n:]
+
+    # block_size = 8
+    # print(train_data[:block_size+1])
+
+    # x = train_data[:block_size]
+    # y = train_data[1:block_size+1]
+    # for t in range(block_size):
+    #     context = x[:t+1]
+    #     target = y[t]
+    #     print(f'{context} -> {target}')
+
+    torch.manual_seed(1337)
+    batch_size = 4 # how many independent sequences will we process in parallel?
+    block_size = 8 # what is the maximum context length for predictions?
+
+    def get_batch(split):
+        # generate a small batch of data of inputs x and targets y
+        data = train_data if split == 'train' else val_data
+        ix = torch.randint(len(data) - block_size, (batch_size,))
+        x = torch.stack([data[i:i+block_size] for i in ix])
+        y = torch.stack([data[i+1:i+block_size+1] for i in ix])
+        return x, y
+
+    xb, yb = get_batch('train')
+    print('inputs:')
+    print(xb.shape)
+    print(xb)
+    print('targets:')
+    print(yb.shape)
+    print(yb)
+
+    print('----')
+
+    for b in range(batch_size): # batch dimension
+        for t in range(block_size): # time dimension
+            context = xb[b, :t+1]
+            target = yb[b,t]
+            print(f"when input is {context.tolist()} the target: {target}")
+
+if __name__ == '__main__':
+    main()
